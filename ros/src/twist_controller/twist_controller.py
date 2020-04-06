50b3aad5a0bfaffbb3289f56c098b900f8df1e14
Marco Rossi <marco.rossi4@fcagroup.com>
2020-04-06 17:34:12 +0200

    feat: add None check also for dbw_enable

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -10,17 +10,17 @@ ONE_MPH = 0.44704
 class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit,
         accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
-        
+
         # TODO: Implement
         self.yaw_controller =  YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
 
         kp = 0.3
         ki = 0.1
-        kd = 0.
-        mn = 0. #Minimum throttle value
-        mx = 0.2 # max throttle value
-        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+        kd  = 0.022
+        mn  = 0.0 #Minimum throttle value
+        mx = 0.4 # max throttle value
 
+        self.throttle_controller = PID(kp , ki, kd , mn , mx)
         tau = 0.5 # (1/(2pi*tau) = cutoff frequency
         ts = .02 # sampling time
         self.vel_lpf = LowPassFilter(tau, ts)
@@ -40,7 +40,6 @@ class Controller(object):
         if not dbw_enabled:
             self.throttle_controller.reset()
             return 0., 0., 0.
-        
         current_vel = self.vel_lpf.filt(current_vel)
 
         # some commented code
@@ -52,9 +51,6 @@ class Controller(object):
 
         current_time = rospy.get_time()
         sample_time = current_time - self.last_time
-
-        #rospy.loginfo('Sampling time = %s', sample_time)
-
         self.last_time = current_time
 
         throttle = self.throttle_controller.step(vel_error, sample_time)
