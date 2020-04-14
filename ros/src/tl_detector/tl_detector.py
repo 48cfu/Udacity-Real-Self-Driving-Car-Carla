23623143340dcb55ed0ae24b08e7e8caa9da4172
Marco Rossi <marco.rossi4@fcagroup.com>
2020-04-14 20:14:38 +0200

    fix: fix image encoding from camera to rgb8

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -124,7 +124,7 @@ class TLDetector(object):
             self.prev_light_loc = None
             return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "rgb8")
 
 
         # return light.state
